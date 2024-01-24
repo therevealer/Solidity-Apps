f7c45017ae62f5762acd2835a8bc47149e2859b8
therevealer <officialnfluence@gmail.com>
2024-01-24 23:23:59 +0100

    Events

--- wallet/multiSigwallet.sol
+++ wallet/multiSigwallet.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event submitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+
+    event confirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeTransaction(address indexed owner, uint indexed txIndex);
+    event executeTransaction(address indexed sender, uint indexed txIndex);
+}
+
+
 
 
 
