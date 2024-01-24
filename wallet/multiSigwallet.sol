d765137d33453d481a1a49ba52addf3cbc41243a
therevealer <officialnfluence@gmail.com>
2024-01-24 23:03:57 +0100

    MultiSig Specifications

---
+++ wallet/multiSigwallet.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+
+
+
+// The wallet owners can
+
+//submit a transaction
+//approve and revoke approval of pending transactions
+//anyone can execute a transaction after enough owners has approved it.
