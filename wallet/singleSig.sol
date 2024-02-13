15ac6d5cf8b30369fc44a8aaa709b6a9ebe1a4ba
therevealer <officialnfluence@gmail.com>
2024-02-13 03:20:54 +0100

    Completed Single Sig Wallet

--- wallet/singleSig.sol
+++ wallet/singleSig.sol
@@ -1,3 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+contract singleSigWallet {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event Withdraw(uint amount);
+
+    address public owner;
+    uint public balance;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    constructor (){
+        owner = msg.sender;
+    }
+
+    function deposit(uint amount) public payable {
+        balance += msg.value;
+
+        emit Deposit(msg.sender, amount, balance);
+    }
+
+    function withdraw(uint amount) public onlyOwner {
+        require(amount <= balance, "Insufficient balance");
+        balance -= amount;
+        payable(owner).transfer(amount);
+
+        emit Withdraw(amount);
+    }
+}
